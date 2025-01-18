b80f64c0705379b9c4bfa39e82067575837c7120
Ty <nicholsty72@gmail.com>
2025-01-17 19:56:10 -0600

    added components for main page and contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "../components/contact-form";
+
+const ContactPage: React.FC = () => {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ src/app/services/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ServicePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>
+        Welcome to our service page. Here you can find all the services we
+        offer.
+      </p>
+      {/* Add more content about your services here */}
+    </div>
+  );
+};
+
+export default ServicePage;
